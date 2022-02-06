639abf2ee877b6c708a83e548a7a949256290bd7
Otybrian <otieno.brian@student.moringaschool.com>
2022-02-07 01:54:16 +0300

    call function submitQuote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
 
     showQuoteDetails: boolean;
 
-    constructor(public id: number, public quoteName: string, public quoteDetails: string, 
+    constructor(public id: number, public quoteName: string, public quoteDetails: string, public postedBy:string,
         public datePosted: Date){
         this.showQuoteDetails=false;
     }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quote:Quote[] = [
-    new Quote (1, 'Yes we can!', 'Barrack Obama', new Date (2022,2,6)),
-    new Quote (2, 'The higher you go the cooler it becomes','Geographers', new Date (2022,2,6)),
+    new Quote (1, 'Yes we can!', 'Barrack Obama','Brian', new Date (2022,2,6)),
+    new Quote (2, 'The higher you go the cooler it becomes','Geographers','Brian', new Date (2022,2,6)),
   ];
 
   toggleDetails(index:number){
@@ -23,6 +23,13 @@ export class QuoteComponent implements OnInit {
       confirm('Do you want to delete this quote?')
       this.quote.splice(index, 1);
     }
+  }
+
+  addNewQuote(quote:any){
+    let quoteLength = this.quote.length;
+    quote.id = quoteLength + 1;
+    quote.quoteName = new quote()
+    this.quote.push(quote)
   }
 
   constructor() { }
